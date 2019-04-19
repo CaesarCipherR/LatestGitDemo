1a3a5812eae88c4f0cc7b70dce322442e6447181
Mariana <nevkrytam@hotmail.com>
2019-04-19 14:32:09 -0400

    Git commit

---
+++ src/test/java/SmokeTest/validateTitle.java
@@ -0,0 +1,42 @@
+package SmokeTest;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.Base;
+
+public class validateTitle extends Base {
+	public static Logger log = LogManager.getLogger(Base.class.getName());
+	@BeforeTest
+	public void initialize() throws IOException {
+		driver=initializeDriver();
+		log.info("Driver is initialized");
+		driver.get(prop.getProperty("url"));
+	    log.info("Navigated to the home page");
+	}
+	
+	
+	@Test
+	public void validateAppTitle() throws IOException {
+
+		LandingPage lp = new LandingPage(driver);
+		Assert.assertEquals("Facebook",lp.getTitle().getText());
+		log.info("Successfully validated Title");
+
+		
+	}
+	@AfterTest
+	public void teardown() {
+		driver.close();
+		driver=null;
+	}
+}
---
+++ target/classes/target/classes/target/classes/src/test/java/SmokeTest/HomePage.java
@@ -0,0 +1,53 @@
+package SmokeTest;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.Base;
+
+public class HomePage extends Base {	
+	public static Logger log = LogManager.getLogger(Base.class.getName());
+	@Test(dataProvider="getData")
+	public void basePageNavigation(String Username, String Password, String text) throws IOException {
+		driver=initializeDriver();
+		driver.get(prop.getProperty("url"));
+		LandingPage lp = new LandingPage(driver);
+		lp.getEmail().sendKeys(Username);
+		lp.getPassword().sendKeys(Password);
+		log.info(text);
+		LoginPage l = new LoginPage(driver);
+		l.getLogin().click();
+		
+	}
+
+	@AfterTest
+	public void teardown() {
+		driver.close();
+		driver=null;
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data = new Object[2][3];
+		data[0][0]="nonerestrictiveuser@facebook.com";
+		data[0][1]="123456";
+		data[0][2]="None Restrictive User";
+		
+		data[1][0]="restrictiveuser@facebook.com";
+		data[1][1]="678910";
+		data[1][2]="Restrictive User";
+		
+		return data;
+		
+	}
+	
+
+}
